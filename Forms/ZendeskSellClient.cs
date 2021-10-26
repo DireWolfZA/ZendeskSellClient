3c7f5ed6c0dc1aaec17bdbb3b072eb9ef59bc770
░▒▓█│【Walkman】│█▓▒░ <MatthewC@direwolf.co.za>
2021-10-26 15:59:38 +0200

    added applying dark theme

--- Forms/ZendeskSellClient.cs
+++ Forms/ZendeskSellClient.cs
@@ -7,6 +7,24 @@ namespace Forms {
     public partial class ZendeskSellClient : Form {
         public ZendeskSellClient() {
             InitializeComponent();
+
+            ApplyTheme();
+        }
+
+        void ApplyTheme() {
+            var theme = WalkmanLib.Theme.Dark;
+            ApplyTheme(this);
+            if (components != null)
+                ApplyTheme(components.Components);
+            ToolStripManager.Renderer = new WalkmanLib.CustomPaint.ToolStripSystemRendererWithDisabled(theme.ToolStripItemDisabledText);
+        }
+        public static void ApplyTheme(Form form) {
+            var theme = WalkmanLib.Theme.Dark;
+            WalkmanLib.ApplyTheme(theme, form, true);
+        }
+        public static void ApplyTheme(System.Collections.IEnumerable controls) {
+            var theme = WalkmanLib.Theme.Dark;
+            WalkmanLib.ApplyTheme(theme, controls, true);
         }
 
     }
