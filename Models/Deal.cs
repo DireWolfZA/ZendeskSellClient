7274086e4f9ec49a0fb2f367f380459c65d07ae2
░▒▓█│【Walkman】│█▓▒░ <MatthewC@direwolf.co.za>
2021-10-28 13:59:50 +0200

    updated ZendeskSellApi, updated Deal to API definition

--- Models/Deal.cs
+++ Models/Deal.cs
@@ -1,15 +1,15 @@
 namespace Models {
     public class Deal : Base {
         public string LastActivityAt { get; set; }
-        public string LastStageChangeByID { get; set; }
+        public int? LastStageChangeByID { get; set; }
         public string DropboxEmail { get; set; }
-        public int OrganizationID { get; set; }
+        public int? OrganizationID { get; set; }
 
         public string Value { get; set; }
         public float GetValue() => float.Parse(Value, System.Globalization.NumberFormatInfo.InvariantInfo);
 
         public string Name { get; set; }
-        public int? ContactID { get; set; }
+        public int ContactID { get; set; }
         public string Currency { get; set; }
         public bool Hot { get; set; }
         public int? StageID { get; set; }
