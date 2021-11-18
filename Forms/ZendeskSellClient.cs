ab745f714f4746f325e50d0f7d3a1fc7ce37ef5a
░▒▓█│【Walkman】│█▓▒░ <MatthewC@direwolf.co.za>
2021-11-18 20:50:47 +0200

    added storing & getting data, and setting propertygrids on type change

--- Forms/ZendeskSellClient.cs
+++ Forms/ZendeskSellClient.cs
@@ -33,9 +33,52 @@ namespace Forms {
         }
 
         private ZendeskSell.IZendeskSellClient sellClient;
+        private IEnumerable<ZendeskSell.CustomFields.CustomFieldResponse> leadCustomFields;
+        private IEnumerable<ZendeskSell.CustomFields.CustomFieldResponse> contactCustomFields;
+        private IEnumerable<ZendeskSell.CustomFields.CustomFieldResponse> dealCustomFields;
+        private Dictionary<int, string> users;
+        private Dictionary<int, string> leadSources;
+        private Dictionary<int, string> dealSources;
+        private Dictionary<int, string> dealStages;
+        private Dictionary<int, string> dealLossReasons;
+        private Dictionary<int, string> dealUnqualifiedReasons;
+        private Dictionary<int, string> products;
 
-        private void cbxType_SelectedIndexChanged(object sender, EventArgs e) {
+        private async void cbxType_SelectedIndexChanged(object sender, EventArgs e) {
+            cbxType.Enabled = false;
+            grpMain.Enabled = false;
 
+            lstItems.Items.Clear();
+
+            try {
+                users = (await ZendeskGet.GetAll((pn, pc) => sellClient.Users.GetAsync(pn, pc))).ToDictionary(u => u.ID, u => u.Name);
+
+                switch (cbxType.Text) {
+                    case "Leads":
+                        leadCustomFields = await ZendeskGet.GetAll((pn, pc) => sellClient.CustomFields.GetLeads());
+                        leadSources = (await ZendeskGet.GetAll((pn, pc) => sellClient.LeadSources.GetAsync(pn, pc))).ToDictionary(s => s.ID, s => s.Name);
+                        SetPropertyGrid(new LeadPropertyGrid(leadCustomFields, users, leadSources));
+                        break;
+                    case "Contacts":
+                        contactCustomFields = await ZendeskGet.GetAll((pn, pc) => sellClient.CustomFields.GetContacts());
+                        SetPropertyGrid(new ContactPropertyGrid(contactCustomFields, users));
+                        break;
+                    case "Deals":
+                        dealCustomFields = await ZendeskGet.GetAll((pn, pc) => sellClient.CustomFields.GetDeals());
+                        dealSources = (await ZendeskGet.GetAll((pn, pc) => sellClient.DealSources.GetAsync(pn, pc))).ToDictionary(s => s.ID, s => s.Name);
+                        dealStages = (await ZendeskGet.GetAll((pn, pc) => sellClient.Stages.GetAsync(pn, pc))).ToDictionary(s => s.ID, s => s.Name);
+                        dealLossReasons = (await ZendeskGet.GetAll((pn, pc) => sellClient.DealLossReasons.GetAsync(pn, pc))).ToDictionary(s => s.ID, s => s.Name);
+                        dealUnqualifiedReasons = (await ZendeskGet.GetAll((pn, pc) => sellClient.DealUnqualifiedReasons.GetAsync(pn, pc))).ToDictionary(s => s.ID, s => s.Name);
+                        var contacts = (await ZendeskGet.GetAll((pn, pc) => sellClient.Contacts.GetAsync(pn, pc))).ToDictionary(c => c.ID, c => c.Name);
+                        SetPropertyGrid(new DealPropertyGrid(dealCustomFields, users, contacts, dealSources, dealStages, dealLossReasons, dealUnqualifiedReasons));
+                        break;
+                }
+                grpMain.Enabled = true;
+            } finally {
+                cbxType.Enabled = true;
+                btnUpdate.Enabled = false;
+                btnDelete.Enabled = false;
+            }
         }
 
 
