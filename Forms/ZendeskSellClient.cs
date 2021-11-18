1cb800a2652eaa2f7336f0e636612099fc56a0e9
░▒▓█│【Walkman】│█▓▒░ <MatthewC@direwolf.co.za>
2021-11-18 20:49:14 +0200

    added helper methods to get/set propertygrids and listviewitems

--- Forms/ZendeskSellClient.cs
+++ Forms/ZendeskSellClient.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Controls;
 using Helpers;
 
 namespace Forms {
@@ -36,6 +37,32 @@ namespace Forms {
         private void cbxType_SelectedIndexChanged(object sender, EventArgs e) {
 
         }
+
+
+        private void SetPropertyGrid<T>(IZendeskPropertyGrid<T> grid) where T : Models.Base {
+            if (scMain.Panel2.Tag != null)
+                scMain.Panel2.Controls.RemoveAt(0);
+
+            grid.Dock = DockStyle.Fill;
+            scMain.Panel2.Tag = grid;
+            scMain.Panel2.Controls.Add(grid);
+        }
+        private IZendeskPropertyGrid<T> GetPropertyGrid<T>() where T : Models.Base =>
+            (IZendeskPropertyGrid<T>)scMain.Panel2.Tag;
+
+        private ListViewItem UpdateItem(ListViewItem item, Models.Base data) {
+            item.Tag = data;
+            item.Text = data.ID.ToString();
+            item.SubItems[1].Text = data.Name;
+            item.SubItems[2].Text = users[data.OwnerID.Value];
+
+            return item;
+        }
+        private ListViewItem CreateItem(Models.Base data) =>
+            UpdateItem(new ListViewItem(new string[3]), data);
+        private Models.Base GetData(ListViewItem item) =>
+            (Models.Base)item.Tag;
+
 
         private void btnGetAll_Click(object sender, EventArgs e) {
 
