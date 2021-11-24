acee70c342ddb44bb528a9fda971b3e2e33e8179
░▒▓█│【Walkman】│█▓▒░ <MatthewC@direwolf.co.za>
2021-11-24 11:06:47 +0200

    moved images to Images subfolder, added Settings and Refresh images

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -71,6 +71,26 @@ namespace Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Refresh {
+            get {
+                object obj = ResourceManager.GetObject("Refresh", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Settings {
+            get {
+                object obj = ResourceManager.GetObject("Settings", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon ZendeskSell {
